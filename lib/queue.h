966dd329b883a0add5680e5368ea70ac9fff558c
YEAHx4 <developerstarlight@gmail.com>
2025-05-24 23:38:45 +0900

    chore: remove redundant comments

--- lib/queue.h
+++ lib/queue.h
@@ -1,19 +1,16 @@
 #ifndef C_PROGRAMMING_QUEUE_H
 #define C_PROGRAMMING_QUEUE_H
 
-// 큐 노드 구조체
 typedef struct Node {
   int data;
   struct Node* next;
 } Node;
 
-// 큐 구조체
 typedef struct Queue {
   Node* front;
   Node* rear;
 } Queue;
 
-// 함수 선언
 void initQueue(Queue* q);
 int isEmpty(Queue* q);
 void enqueue(Queue* q, int value);
